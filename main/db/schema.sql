860eb94ba431cde6917f4dd407311730b651bac5
jorjik81 <jorjik81@gmail.com>
2023-10-18 07:32:17 +0400

    Schema added

---
+++ main/db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_manager;
+CREATE DATABASE employee_manager;
+
+use employee_manager;
+
+create table department( 
+id int not null  AUTO_INCREMENT PRIMARY KEY,
+name varchar(30)
+);
+
+create table roles( 
+id int not null AUTO_INCREMENT PRIMARY KEY,
+title varchar(30),
+salary decimal,
+department_id int,
+foreign key (department_id)
+references department(id)
+ON DELETE SET NULL
+);
+
+create table employee(
+id int not null AUTO_INCREMENT PRIMARY KEY,
+first_name varchar(30),
+last_name varchar(30),
+role_id int,
+manager_id int,
+foreign key (role_id)
+references roles(id)
+ON DELETE SET NULL,
+foreign key (manager_id) 
+references employee(id)
+on delete set null
+); 
